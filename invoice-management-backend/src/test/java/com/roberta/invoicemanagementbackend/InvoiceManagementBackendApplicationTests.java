6aa1888031a0fe90bef172019e4886e0920ba1a5
roberta <116081834+robertamanta@users.noreply.github.com>
2023-07-13 01:26:01 +0300

    created frontend and backend folders

---
+++ invoice-management-backend/src/test/java/com/roberta/invoicemanagementbackend/InvoiceManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberta.invoicemanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
